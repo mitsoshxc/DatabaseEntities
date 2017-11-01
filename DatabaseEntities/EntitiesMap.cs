0f1014ea2c8fae8de6d97829285a0e7b3de17e5b
mitsoshxc <mitsoshxc@gmail.com>
2017-11-01 16:05:33 +0200

    Initial commit

---
+++ DatabaseEntities/EntitiesMap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+
+public partial class MainWindow
+{
+    Dictionary<string, string> EntitiesDictionary = new Dictionary<string, string>()
+        {
+            {"bigint", "int"},
+            {"int", "int"},
+            {"smallint", "int"},
+            {"char", "string"},
+            {"nchar", "string"},
+            {"ntext", "string"},
+            {"nvarchar", "string"},
+            {"text", "string"},
+            {"varchar", "string"},
+            {"decimal", "decimal"},
+            {"float", "decimal"},
+            {"money", "decimal"},
+            {"numeric", "decimal"},
+            {"real", "decimal"},
+            {"smallmoney", "decimal"},
+            {"date", "DateTime"},
+            {"datetime", "DateTime"},
+            {"datetime2", "DateTime"},
+            {"smalldatetime", "DateTime"}
+        };
+}
---
+++ DatabaseEntities/Entity.cs
@@ -0,0 +1,18 @@
+public class Test
+{
+    public string Code { get; set; }
+    public string LineId { get; set; }
+    public string Object { get; set; }
+    public string LogInst { get; set; }
+    public string U_AccYear { get; set; }
+    public string U_Layer { get; set; }
+    public string U_LayerName { get; set; }
+    public string U_Type { get; set; }
+    public string U_Reinsurer { get; set; }
+    public string U_LineCap { get; set; }
+    public string U_Ceded { get; set; }
+    public string U_Retention { get; set; }
+    public string U_ComType { get; set; }
+    public string U_Commision { get; set; }
+    public string U_Reduction { get; set; }
+}
